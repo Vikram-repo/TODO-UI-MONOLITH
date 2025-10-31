91eca67bffec1e1fd68749a3cd9b68032fc17698
Vikram-repo <vikramazure02@gmail.com>
2025-11-01 01:32:45 +0530

    Update TodoApp.js

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -4,7 +4,7 @@ import { Button, TextField, Container, Typography, Grid, Card, CardContent, Icon
 import { Delete } from '@mui/icons-material';
 import { Box } from '@mui/material';
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'http://20.39.143.34:8000';
 
 const backgroundImage = process.env.PUBLIC_URL + '/background.jpg';
 
